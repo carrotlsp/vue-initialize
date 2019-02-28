26b8dd0268a86c7c3d1b9909f6f3954cd46636e4
carrot__lsp <476078307@qq.com>
2019-02-28 10:47:25 +0800

    集成 fastclick 加快点击事件处理

--- src/main.js
+++ src/main.js
@@ -4,12 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+import FastClick from 'fastclick'
 import "./assets/css/iconfont.css"
 import "./assets/css/1px.css"
 import "./assets/css/reset.css"
 import 'swiper/dist/css/swiper.css'
 
-
+FastClick.attach(document.body)
 
 Vue.use(VueAwesomeSwiper)
 
--- src/views/city/componenets/AzIndex.vue
+++ src/views/city/componenets/AzIndex.vue
@@ -1,16 +1,60 @@
 <template>
-  <div class="wrapper">
-    <div @click="handleClick(key)" v-for="(item, key) in cityList" :key="key" class="letter">{{key}}</div>
+  <div
+    ref="wrapper"
+    @touchstart="handleTouchStart"
+    @touchmove="handleTouchMove"
+    @touchend="handleTouchEnd"
+    class="wrapper"
+  >
+    <div
+      :ref="key"
+      @click="handleClick(key)"
+      v-for="(item, key) in cityList"
+      :key="key"
+      class="letter"
+    >{{key}}</div>
   </div>
 </template>
 <script>
 export default {
-  props:{
-    cityList:[Object,Array]
+  props: {
+    cityList: [Object, Array]
   },
-  methods:{
-    handleClick(key){
-      this.$emit('changeLetter',key);
+  data() {
+    return {
+      aOffsetTop: 0
+    };
+  },
+  computed: {
+    letterArray() {
+      let array = [];
+      for (const key in this.cityList) {
+        array.push(key);
+      }
+      return array;
+    }
+  },
+  methods: {
+    handleClick(key) {
+      this.$emit("changeLetter", key);
+    },
+    handleTouchStart(e) {
+      if (this.aOffsetTop === 0) {
+        this.aOffsetTop = this.$refs["A"][0].offsetTop;
+      }
+    },
+    handleTouchMove(event) {
+      let touch = event.touches[0]; //获取第一个触点
+      let index = Math.floor((touch.clientY - this.aOffsetTop) / 20);
+      this.handleClick(this.letterArray[index]);
+
+      // console.log(touch.clientY)
+      // console.log(this.$refs['wrapper'].offsetTop)
+      console.log("handleTouchMove");
+    },
+    handleTouchEnd(e) {
+      console.log(e);
+      console.log("handleTouchEnd");
     }
   }
 };
@@ -28,8 +72,7 @@ export default {
   align-items center
   color blue
   font-size 14px
-  .letter{
+  .letter
     height 20px
     line-height 20px
-  }
 </style>
